e713398b19bfbe2403206a43d5c18a08b6ec1cde
AlpYksl <alpuksl@gmail.com>
2022-05-08 18:53:01 +0300

    ImageRegistration

--- ImageRegistration.py
+++ ImageRegistration.py
@@ -1,31 +1,30 @@
 import cv2
 import numpy as np
-
+import matplotlib.pyplot as plt
+import FeatureDetector as fd
 # Open the image files.
-img1_color = cv2.imread("ImageQuery/im1.jpg")  # Image to be aligned.
-img2_color = cv2.imread("ImageQuery/im2.jpg")  # Reference image.
+img1 = cv2.imread("ImageMatch/osymkitapcik.jpg")  # Image to be aligned.
+img2= cv2.imread("ImageMatch/Kitap_202006271656344909921.jpg")  # Reference image.
 
 # Convert to grayscale.
-img1 = cv2.cvtColor(img1_color, cv2.COLOR_BGR2GRAY)
-img2 = cv2.cvtColor(img2_color, cv2.COLOR_BGR2GRAY)
-height, width = img2.shape
+img1_color = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+img2_color = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
 
-# Create ORB detector with 5000 features.
-orb_detector = cv2.ORB_create(5000)
+# use ORB to detect keypoints and extract (binary) local
+# invariant features
+orb = cv2.ORB_create(500)
+(kpsA, descsA) = orb.detectAndCompute(img1_color, None)
+(kpsB, descsB) = orb.detectAndCompute(img2_color, None)
 
-# Find keypoints and descriptors.
-# The first arg is the image, second arg is the mask
-#  (which is not required in this case).
-kp1, d1 = orb_detector.detectAndCompute(img1, None)
-kp2, d2 = orb_detector.detectAndCompute(img2, None)
+# match the features
+method = cv2.DESCRIPTOR_MATCHER_BRUTEFORCE_HAMMING
+matcher = cv2.DescriptorMatcher_create(method)
+matches = matcher.match(descsA, descsB, None)
 
 # Match features between the two images.
 # We create a Brute Force matcher with
 # Hamming distance as measurement mode.
 matcher = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-
-# Match the two sets of descriptors.
-matches = matcher.match(d1, d2)
 
 # Sort matches on the basis of their Hamming distance.
 #matches.sort(key=lambda x: x.distance)
@@ -38,17 +37,47 @@ no_of_matches = len(matches)
 p1 = np.zeros((no_of_matches, 2))
 p2 = np.zeros((no_of_matches, 2))
 
-for i in range(len(matches)):
-    p1[i, :] = kp1[matches[i].queryIdx].pt
-    p2[i, :] = kp2[matches[i].trainIdx].pt
-
+# loop over the top matches
+for (i, m) in enumerate(matches):
+	# indicate that the two keypoints in the respective images
+	# map to each other
+	p1[i] = kpsA[m.queryIdx].pt
+	p2[i] = kpsB[m.trainIdx].pt
 # Find the homography matrix.
-homography, mask = cv2.findHomography(p1, p2, cv2.RANSAC)
+(H, mask) = cv2.findHomography(p1, p2, method=cv2.RANSAC)
+# use the homography matrix to align the images
+(h, w) = img2_color.shape[:2]
+aligned = cv2.warpPerspective(img1_color, H, (w, h))
 
-# Use this matrix to transform the
-# colored image wrt the reference image.
-transformed_img = cv2.warpPerspective(img1_color,
-                                      homography, (width, height))
+scale_percent = 60  # percent of original size
+width = int(aligned.shape[1] * scale_percent / 100)
+height = int(aligned.shape[0] * scale_percent / 100)
+dim = (width, height)
 
+# resize image
+aligned = cv2.resize(aligned, dim, interpolation=cv2.INTER_AREA)
+template = cv2.resize(img2_color, dim, interpolation=cv2.INTER_AREA)
+
+# our first output visualization of the image alignment will be a
+# side-by-side comparison of the output aligned image and the
+# template
+stacked = np.hstack([aligned, template])
+
+# our second image alignment visualization will be *overlaying* the
+# aligned image on the template, that way we can obtain an idea of
+# how good our image alignment is
+overlay = template.copy()
+output = aligned.copy()
+cv2.addWeighted(overlay, 0.5, output, 0.5, 0, output)
+# show the two output image alignment visualizations
+cv2.imshow("Image Alignment Stacked", stacked)
+cv2.imshow("Image Alignment Overlay", output)
+cv2.waitKey(0)
+#plt.subplot(2,2,1)
+#plt.imshow(fd.ORB("ImageMatch/osymkitapcik.jpg","output.jpg"))
+#plt.subplot(2,2,2)
+#plt.imshow(fd.ORB("ImageMatch/osymkitapcik.jpg","ImageMatch/Kitap_202006271656344909921.jpg"))
+#plt.show()
 # Save the output.
-cv2.imwrite('output.jpg', transformed_img)
+cv2.imwrite('output.jpg', aligned)
+
--- main.py
+++ main.py
@@ -1,53 +1,24 @@
 import FlannMatch as fl
 import SiftMatch as sm
 import FeatureDetector as fd
-import  HarrisCornerDetector as hcd
+import HarrisCornerDetector as hcd
+import SurfCornerDetector as scd
 from matplotlib import pyplot as plt
 
-#plt.subplot(1,2,1);plt.imshow(fl.Flann("ImageQuery/Fenerbahce.jpg","ImageQuery/fb-gs.jpg"));plt.title("Flann")
-#plt.subplot(1,2,2);plt.imshow(sm.Sift("ImageQuery/Fenerbahce.jpg","ImageQuery/fb-gs.jpg")) ;plt.title("Sift")
-#plt.subplot(2,2,1);plt.imshow(hcd.HarrisCorner("ImageQuery/Fenerbahce.jpg")) ;plt.title("Harris");
-#plt.subplot(2,2,2);plt.imshow(fd.ORB("ImageQuery/Fenerbahce.jpg","ImageQuery/fb-gs.jpg"));plt.title("ORB");
-#plt.show()
-#plt.imshow(sfd.Surf("ImageQuery/Fenerbahce.jpg")); plt.show()
 
 
-
-# create figure
-fig = plt.figure(figsize=(10, 7))
-
-# setting values to rows and column variables
-rows = 2
-columns = 2
-
-# Adds a subplot at the 1st position
-fig.add_subplot(rows, columns, 1)
-
-# showing image
+rows, cols = 2, 2
+plt.subplot(rows, cols, 1)
 plt.imshow(fl.Flann("ImageQuery/Fenerbahce.jpg","ImageQuery/fb-gs.jpg"))
-plt.axis('off')
-plt.title("First")
-
-# Adds a subplot at the 2nd position
-fig.add_subplot(rows, columns, 2)
-
-# showing image
+plt.title("Flann")
+plt.subplot(rows, cols, 2)
 plt.imshow(sm.Sift("ImageQuery/Fenerbahce.jpg","ImageQuery/fb-gs.jpg"))
-plt.axis('off')
-plt.title("Second")
-
-# Adds a subplot at the 3rd position
-fig.add_subplot(rows, columns, 3)
-
-# showing image
+plt.title("Sift")
+plt.subplot(rows, cols, 3)
 plt.imshow(hcd.HarrisCorner("ImageQuery/Fenerbahce.jpg"))
-plt.axis('off')
-plt.title("Third")
-
-# Adds a subplot at the 4th position
-fig.add_subplot(rows, columns, 4)
-
-# showing image
+plt.title("HarrisCorner")
+plt.subplot(rows, cols, 4)
 plt.imshow(fd.ORB("ImageQuery/Fenerbahce.jpg","ImageQuery/fb-gs.jpg"))
-plt.axis('off')
-plt.title("Fourth")
+plt.title("ORB")
+
+plt.show()
